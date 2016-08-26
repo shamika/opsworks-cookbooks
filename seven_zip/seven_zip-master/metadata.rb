b2e2570746521c421013d26bbf49a3bbb8074c85
shamika <shamika@wso2.com>
2016-08-26 17:59:03 -0600

    Adding Cookbook dependency

---
+++ seven_zip/seven_zip-master/metadata.rb
@@ -0,0 +1,11 @@
+name             'seven_zip'
+maintainer       'Shawn Neal'
+maintainer_email 'sneal@sneal.net'
+issues_url       'https://github.com/windowschefcookbooks/seven_zip/issues'
+source_url       'https://github.com/windowschefcookbooks/seven_zip'
+license          'Apache 2.0'
+description      'Installs/Configures the 7-zip file archiver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '2.0.2'
+supports         'windows'
+depends          'windows', '>= 1.2.2'
