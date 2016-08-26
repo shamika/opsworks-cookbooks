78915b749ea0b80c391d16d9897d28ee4d00d6b1
shamika <shamika@wso2.com>
2016-08-26 16:46:17 -0600

    Adding Cookbook dependency

---
+++ git/metadata.rb
@@ -0,0 +1,36 @@
+name 'git'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs git and/or sets up a Git server daemon'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '4.6.0'
+recipe 'git', 'Installs git'
+recipe 'git::server', 'Sets up a runit_service for git daemon'
+recipe 'git::source', 'Installs git from source'
+
+supports 'amazon'
+supports 'arch'
+supports 'centos'
+supports 'debian'
+supports 'fedora'
+supports 'freebsd'
+supports 'mac_os_x', '>= 10.6.0'
+supports 'omnios'
+supports 'oracle'
+supports 'redhat'
+supports 'smartos'
+supports 'scientific'
+supports 'suse'
+supports 'opensuse'
+supports 'opensuseleap'
+supports 'ubuntu'
+supports 'windows'
+
+depends 'build-essential'
+depends 'dmg'
+depends 'windows'
+depends 'yum-epel'
+
+source_url 'https://github.com/chef-cookbooks/git' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/git/issues' if respond_to?(:issues_url)
